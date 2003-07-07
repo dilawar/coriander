7760202098486eb0f0c0638bb59510eafec800c3
ddouxchamps <ddouxchamps>
2003-07-07 08:50:43 +0000

    fixed absolute entry sensitivity

--- src/update_ranges.c
+++ src/update_ranges.c
@@ -102,8 +102,9 @@ UpdateRange(int feature)
     break;
   }
   // all features: set absolute range sensitivity:
-  abs_is_on=(camera->feature_set.feature[feature-FEATURE_MIN].absolute_capable&& // abs control is on
-	     camera->feature_set.feature[feature-FEATURE_MIN].abs_control);
+  abs_is_on=((camera->feature_set.feature[feature-FEATURE_MIN].absolute_capable&& // abs control is on
+	      camera->feature_set.feature[feature-FEATURE_MIN].abs_control) &&
+	     camera->feature_set.feature[feature-FEATURE_MIN].is_on);
   if (camera->feature_set.feature[feature-FEATURE_MIN].absolute_capable) {
     sprintf(stemp,"feature_%d_abs_entry",feature);
     gtk_widget_set_sensitive(lookup_widget(main_window, stemp), abs_is_on);
